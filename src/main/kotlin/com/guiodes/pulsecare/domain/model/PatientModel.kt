fd3871fed034c8f37ad1f2fe69af6e6f930efa02
GuiOdes <guilherme.montnezes@gmail.com>
2024-06-26 22:03:14 -0300

    Patient controller

--- src/main/kotlin/com/guiodes/pulsecare/domain/model/PatientModel.kt
+++ src/main/kotlin/com/guiodes/pulsecare/domain/model/PatientModel.kt
@@ -1,6 +1,7 @@
 package com.guiodes.pulsecare.domain.model
 
 import com.guiodes.pulsecare.domain.enum.GenderEnum
+import jakarta.validation.constraints.NotNull
 import java.time.LocalDate
 
 data class PatientModel(
@@ -9,6 +10,7 @@ data class PatientModel(
     val phone: String? = null,
     val birthDate: LocalDate? = null,
     val document: String? = null,
+    @field:NotNull(message = "This field is required")
     val gender: GenderEnum,
     val medicalHistory: PatientHistoryModel? = null,
     val createdAt: LocalDate? = null
